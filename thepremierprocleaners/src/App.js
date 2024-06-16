8248ada9a09228cd30fa45882c34e94f650f41f0
Sami Samara <samisamara99@yahoo.com>
2024-06-15 21:15:31 -0500

    created repository

---
+++ thepremierprocleaners/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The Premier Pro Cleaner</h1>
+    </div>
+  );
+}
+
+export default App;
