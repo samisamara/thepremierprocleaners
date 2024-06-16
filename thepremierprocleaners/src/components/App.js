fdcec106ad3e9421c1161e6ce9f2ddfe38524a64
Sami Samara <samisamara99@yahoo.com>
2024-06-15 21:30:11 -0500

    added logo, created components folder, moved App.js to componenets folder

--- thepremierprocleaners/src/components/App.js
+++ thepremierprocleaners/src/components/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import '../App.css';
+// import logo from "./images/logo.jpg";
 
 function App() {
   return (
     <div className="App">
       <h1>The Premier Pro Cleaner</h1>
+      {/* <img id="logo" src={logo}/> */}
     </div>
   );
 }
